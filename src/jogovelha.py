689cabd5830e2adfe577cc00e268df0cd8402d67
rolf-gutz <rolf.junior@aluno.faculdadeimpacta.com.br>
2020-04-17 20:52:21 -0300

    forcando erro

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -4,7 +4,7 @@ def inicializar():
         linha = []
         for j in range(3):
             linha.append(".")
-        tab.app end(linha)
+        tab.append(linha)
     return tab
 
 def main( ):
